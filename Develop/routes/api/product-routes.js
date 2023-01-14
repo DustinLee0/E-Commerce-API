ed91f3fa8f2756bc81f1110f1cadcfb96f397aec
Dustin Lee <leedustin@live.com>
2023-01-14 17:16:02 -0500

    bug fix

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -126,7 +126,7 @@ router.delete('/:id', async (req, res) => {
       return;
     }
 
-    res.status(200).json({message: `Product with Id: ${req.params.id} deleted`, catDelete});
+    res.status(200).json({message: `Product with Id: ${req.params.id} deleted`, productDelete});
   } catch (err) {
     res.status(500).json(err);
   }
